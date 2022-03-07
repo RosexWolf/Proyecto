5fc8fbdef434492ac9ac82ce5fe4b48611b06822
Taller 1-202 <Estudiante1202@talleresinpahu.edu>
2022-03-07 10:49:51 -0500

    segunda Actualizacion

--- Enemy.py
+++ Enemy.py
@@ -1,1 +1,34 @@
 import turtle
+
+window=turtle.Screen()
+window.bgcolor('grey')
+
+
+
+class enemy(object):
+    element=turtle.Turtle()
+    position=[]
+    def __init__(self,element,position):
+        self.element=element
+        self.position=position
+    def begin(self):
+        self.element.penup()
+        self.element.shape('turtle')
+        self.element.color('red')
+        self.element.goto(self.position[0],self.position[1])
+        turtle.speed(1)
+    def Move(self): 
+        turtle.position()
+        turtle.forward(25)
+        turtle.position()
+        turtle.backward(-75)
+        turtle.position()
+        
+
+enemy=enemy(turtle.Turtle(),[0,0])
+
+enemy.begin()
+enemy.Move()
+
+
+window.mainloop()
